31c636df1f2bf777f1d868353a57242f0319d338
Eason <craigchismo950@gmail.com>
2025-08-31 17:56:40 +1200

    showing version at the root path to check if deployment is successfuk

--- Program.cs
+++ Program.cs
@@ -57,5 +57,7 @@ app.MapTurnstileEndpoints(); // exposes GET /_turnstile/sitekey for the Swagger 
 app.MapGet("/health", () => Results.Ok(new { status = "ok" }))
    .WithName("Health");
 
+app.MapGet("/", () => "v1");
+
 // Start the app
 app.Run();
